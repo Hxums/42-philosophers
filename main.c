bd1e97e0d9dc31620a09b34a2dd497abf2beb8f5
hcissoko <hcissoko@student.42.fr>
2026-02-18 17:12:23 +0100

    WIP : Philosopers struct

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hcissoko <hcissoko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 08:18:53 by hcissoko          #+#    #+#             */
-/*   Updated: 2026/02/18 15:29:29 by hcissoko         ###   ########.fr       */
+/*   Updated: 2026/02/18 17:11:12 by hcissoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	check_args(int argc, char **argv)
 			if (!ft_isdigit(argv[i][j]) || ft_atol(argv[i]) > INT_MAX)
 			{
 				printf("%s is not a valid parameter\n", argv[i]);
-				exit(0);
+				exit(1);
 			}
 			j++;
 		}
@@ -34,8 +34,43 @@ void	check_args(int argc, char **argv)
 	}
 }
 
+t_philosopher	*gen_philosophers(char **argv)
+{
+	int				i;
+	int				nb_philo;
+	t_philosopher	*list;
+
+	nb_philo = atol(argv[1]);
+	list = malloc(sizeof(t_philosopher) * (nb_philo));
+	if (!list)
+	{
+		printf("Error while creating list of philosophers (malloc issue)\n");
+		exit(1);
+	}
+	i = 0;
+	while (i < nb_philo)
+	{
+		list[i].id = i + 1;
+		list[i].time_to_die = atol(argv[2]);
+		list[i].time_to_eat = atol(argv[3]);
+		list[i].time_to_sleep = atol(argv[4]);
+		if (argv[5])
+			list[i].eating_number_of_times = atol(argv[5]);
+		else
+			list[i].eating_number_of_times = 0;
+		i++;
+	}
+	return (list);
+}
+
 int	main(int argc, char **argv)
 {
+	int				nb_philo;
+	t_philosopher	*list;
+	int				i;
+
+	i = 0;
+	nb_philo = atol(argv[1]);
 	if (argv && argc != 4 && argc != 5)
 	{
 		printf("You can only use it with 4 arguments (+1 optional)\n \
@@ -44,4 +79,13 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	check_args(argc, argv);
+	list = gen_philosophers(argv);
+	while (i < nb_philo)
+	{
+		printf("Philosopher %d\ttime_to_die %d\ttime_to_eat %d\ttime_to_sleep\
+	%d\teating_number_of_times %d\n",
+			list[i].id, list[i].time_to_die, list[i].time_to_eat,
+			list[i].time_to_sleep, list[i].eating_number_of_times);
+			i++;
+	}
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: hcissoko <hcissoko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 01:48:14 by hcissoko          #+#    #+#             */
-/*   Updated: 2026/02/18 15:29:19 by hcissoko         ###   ########.fr       */
+/*   Updated: 2026/02/18 17:00:36 by hcissoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,15 @@
 
 int		ft_isdigit(int c);
 long	ft_atol(char *str);
+
+typedef struct s_philosopher
+{
+	int				id;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eating_number_of_times;
+	pthread_mutex_t	fork;	
+}	t_philosopher;
 
 #endif
