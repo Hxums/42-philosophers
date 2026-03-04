72a2ec36817b399bc005e5d1827a5f957e2689b3
hxums <houmanac@gmail.com>
2026-03-04 20:13:32 +0100

    Improve args checking

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: hcissoko <hcissoko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 15:20:06 by hcissoko          #+#    #+#             */
-/*   Updated: 2026/03/04 09:30:58 by hcissoko         ###   ########.fr       */
+/*   Updated: 2026/03/04 15:57:05 by hcissoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,16 @@ void	check_args(int argc, char **argv)
 	int	i;
 	int	j;
 
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (++i < argc)
 	{
-		j = 0;
-		while (argv[i][j])
+		if (!argv[i][0])
+		{
+			printf("argv[%i] is empty\n", i);
+			exit(1);
+		}
+		j = -1;
+		while (argv[i][++j])
 		{
 			if (!ft_isdigit(argv[i][j]) || ft_atol(argv[i]) > INT_MAX ||
 				ft_atol(argv[i]) <= 0)
@@ -29,12 +34,8 @@ void	check_args(int argc, char **argv)
 				printf("%s is not a valid parameter\n", argv[i]);
 				exit(1);
 			}
-			j++;
 		}
-		i++;
 	}
-	if (ft_atol(argv[1]) == 0)
-		exit(1);
 }
 
 void	check_args_number(int argc)
@@ -42,8 +43,8 @@ void	check_args_number(int argc)
 	if (argc != 5 && argc != 6)
 	{
 		printf("You can only use it with 4 arguments (+1 optional)\n\
-Example : ./philosopers number_of_philosophers time_to_die \
-time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]");
+Example : ./philo number_of_philosophers time_to_die time_to_eat \
+time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
 		exit (1);
 	}
 }
